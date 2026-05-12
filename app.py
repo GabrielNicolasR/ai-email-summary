4810924c7af5e58c8204c9fd7824180f8c653ad3
Gabriel Nicolas <gabrielrochasa@gmail.com>
2026-05-11 23:08:31 -0300

    add enumerate in project

--- app.py
+++ app.py
@@ -9,20 +9,19 @@ client = genai.Client(
     api_key=os.environ.get("GOOGLE_API_KEY")
 )
 chat = client.chats.create(
-    model="models/gemini-2.5-flash"
+    model="models/gemini-2.0-flash"
 ) 
 
 def resumir_emails(lista_de_emails):
-    for email in lista_de_emails :
+    for i, email in enumerate(lista_de_emails) :
         prompt = f"Resuma o seguinte email:{email}"
         
         try:
            resposta = chat.send_message(prompt)
            
-           print(resposta.text)
+           print(f"E-mail {i}: {resposta.text}")
            print("-" * 100)
         except Exception as erro:
            print(f"Erro ao resumir email: {erro}")
 
 resumir_emails(emails)
-
