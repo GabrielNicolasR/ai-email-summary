5d505fd04bda52be56f3621853e08055a6edaa38
Gabriel Nicolas <gabrielrochasa@gmail.com>
2026-05-18 18:49:58 -0300

    feat: upgrade model and save summaries to file

--- app.py
+++ app.py
@@ -9,19 +9,34 @@ client = genai.Client(
     api_key=os.environ.get("GOOGLE_API_KEY")
 )
 chat = client.chats.create(
-    model="models/gemini-2.0-flash"
+    model="models/gemini-2.5-flash"
 ) 
 
 def resumir_emails(lista_de_emails):
-    for i, email in enumerate(lista_de_emails) :
-        prompt = f"Resuma o seguinte email:{email}"
+    
+    lista_de_resumos = []
+    
+    for i, email in enumerate(lista_de_emails, start=1) :
+        prompt = f"""
+        Resuma o seguinte e-mail em poucas linhas:
+
+        {email}
+        """
         
         try:
            resposta = chat.send_message(prompt)
            
            print(f"E-mail {i}: {resposta.text}")
            print("-" * 100)
+           lista_de_resumos.append(f"E-mail {i}: {resposta.text}")
+           
         except Exception as erro:
            print(f"Erro ao resumir email: {erro}")
+    return lista_de_resumos
 
-resumir_emails(emails)
+lista_resumos = resumir_emails(emails)
+
+
+with open("lista-de-resumos.txt", "w", encoding="utf-8") as arquivo:
+    for resumo in lista_resumos :
+        arquivo.write(resumo + "\n")
